f002ca018e1c8d91f5b6204e115b3a68c91dbc87
KaanGaming <kaangaming.gamejolt@gmail.com>
2018-09-04 00:14:03 +0300

    Update VisualStudioWorkloadsDependency.cs

--- source/Cosmos.Build.Builder/Dependencies/VisualStudioWorkloadsDependency.cs
+++ source/Cosmos.Build.Builder/Dependencies/VisualStudioWorkloadsDependency.cs
@@ -36,7 +36,7 @@ namespace Cosmos.Build.Builder.Dependencies
         public async Task InstallAsync(CancellationToken cancellationToken)
         {
             var vsInstallerPath = Environment.ExpandEnvironmentVariables(
-                @"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installershell.exe");
+                @"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vs_installer.exe");
 
             var vsInstancePath = _visualStudioInstance.GetInstallationPath();
             var installedPackages = _visualStudioInstance.GetPackages();
